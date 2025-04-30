acc92ae0b51c5d61f070605d499231841162035f
Nikita <romashko.04@mail.ru>
2025-04-30 15:36:46 +0500

    feat(migration): обновлена базовая миграция в контейнере User

--- app/Containers/UserSection/User/Data/Migrations/2025_04_29_180129_create_users_table.php
+++ app/Containers/UserSection/User/Data/Migrations/2025_04_29_180129_create_users_table.php
@@ -7,14 +7,17 @@ use Illuminate\Support\Facades\Schema;
 return new class() extends Migration {
     public function up(): void
     {
-        Schema::create('users', static function (Blueprint $table) {
+        Schema::create('users_api', static function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
             $table->timestamps();
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('users_api');
     }
 };
